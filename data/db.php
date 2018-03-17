489e9ce9e8297baaf980eacee129524a3bc6c986
Yaakobi <alon.yaakobi@intel.com>
2018-03-17 20:11:22 +0200

    second

---
+++ data/db.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* PDO Singleton Class v.1.0
+*
+* @author Ademílson F. Tonato
+* @link https://twitter.com/ftonato
+*
+*/
+class DB {
+	protected static $instance;
+	protected function __construct() {}
+	public static function getInstance() {
+		if(empty(self::$instance)) {
+			$db_info = array(
+				"db_host" => "localhost",
+				
+				"db_user" => "root",
+				"db_pass" => "",
+				"db_name" => "northwind4");
+			try {
+				self::$instance = new PDO("mysql:host=".$db_info['db_host'].';dbname='.$db_info['db_name'], $db_info['db_user'], $db_info['db_pass']);
+				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);  
+				self::$instance->query('SET NAMES utf8');
+                self::$instance->query('SET CHARACTER SET utf8');
+                //print_r(self::$instance);
+
+            }
+            
+            catch(PDOException $error) {
+				echo $error->getMessage();
+			}
+        }
+		return self::$instance;
+    }
+    
+
+	public static function setCharsetEncoding() {
+		if (self::$instance == null) {
+			self::connect();
+		}
+		self::$instance->exec(
+			"SET NAMES 'utf8';
+			SET character_set_connection=utf8;
+			SET character_set_client=utf8;
+			SET character_set_results=utf8");
+	}
+}
+?>
+
+
